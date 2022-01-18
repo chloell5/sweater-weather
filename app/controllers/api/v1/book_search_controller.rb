2f9676e229844b90d99eba9e6acfdd777b10ac23
ChloeLL <chloell@protonmail.ch>
2022-01-18 11:50:25 -0700

    Limit results by quantity

--- app/controllers/api/v1/book_search_controller.rb
+++ app/controllers/api/v1/book_search_controller.rb
@@ -1,9 +1,7 @@
 class Api::V1::BookSearchController < ApplicationController
   def index
-    books = BookFacade.get_info(params[:location])
-    # if params[:quantity].to_i > 0
-    #   books.books.first(params[:quantity].to_i)
-    # end
-    render json: BooksSerializer.new(books)
+    list = BookFacade.get_info(params[:location])
+    list.books.pop(list.books.length - (params[:quantity].to_i))
+    render json: BooksSerializer.new(list)
   end
 end
