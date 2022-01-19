e767aa20d597f8a48f20343f9ed7e97dcadc94e9
ChloeLL <chloell@protonmail.ch>
2022-01-19 03:03:03 -0700

    Add session compatability

---
+++ spec/requests/api/v1/session_request_spec.rb
@@ -0,0 +1,86 @@
+require 'rails_helper'
+
+describe 'Create session' do
+  let(:headers) { { 'CONTENT_TYPE' => 'application/json' } }
+  before do
+    user_params = {
+      email: 'test@test.com',
+      password: '12345',
+      password_confirmation: '12345'
+    }
+
+    post '/api/v1/users', params: user_params.to_json, headers: headers
+  end
+
+  it 'creates a session' do
+    user_params = {
+      email: 'test@test.com',
+      password: '12345'
+    }
+
+    post '/api/v1/sessions', params: user_params.to_json, headers: headers
+
+    expect(response).to be_successful
+
+    sesh = JSON.parse(response.body, symbolize_names: true)
+
+    expect(sesh).to be_a Hash
+    expect(sesh[:data]).to be_a Hash
+    expect(sesh[:data][:type]).to eq('users')
+    expect(sesh[:data][:attributes]).to be_a Hash
+    expect(sesh[:data][:attributes].length).to eq(2)
+    expect(sesh[:data][:attributes]).to include(:email, :api_keys)
+  end
+
+  it 'fails if wrong password' do
+    user_params = {
+      email: 'test@test.com',
+      password: '1'
+    }
+
+    post '/api/v1/sessions', params: user_params.to_json, headers: headers
+
+    expect(response).to have_http_status(401)
+  end
+
+  it 'fails if no password' do
+    user_params = {
+      email: 'test@test.com'
+    }
+
+    post '/api/v1/sessions', params: user_params.to_json, headers: headers
+
+    expect(response).to have_http_status(401)
+  end
+
+  it 'fails if no email' do
+    user_params = {
+      password: '12345'
+    }
+
+    post '/api/v1/sessions', params: user_params.to_json, headers: headers
+
+    expect(response).to have_http_status(401)
+  end
+
+  it 'fails if invalid email' do
+    user_params = {
+      email: 'test test.com',
+      password: '12345'
+    }
+
+    post '/api/v1/sessions', params: user_params.to_json, headers: headers
+
+    expect(response).to have_http_status(401)
+  end
+
+  it 'fails if no password' do
+    user_params = {
+      email: 'test@test.com'
+    }
+
+    post '/api/v1/sessions', params: user_params.to_json, headers: headers
+
+    expect(response).to have_http_status(401)
+  end
+end
