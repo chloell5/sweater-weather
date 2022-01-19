d7f1b075b86e08184aa2f3fef8b97ac97456d50d
ChloeLL <chloell@protonmail.ch>
2022-01-19 02:45:26 -0700

    Modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
 
   namespace :api do
    namespace :v1 do
-     get '/forecast', to: '/api/v1/forecast#index'
+     resources :forecast, only: :index
      get '/backgrounds', to: '/api/v1/background#index'
      resources :users, only: :create
+     resources :sessions, only: :create
    end
  end
 end
