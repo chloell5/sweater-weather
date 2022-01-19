6931c0ae7f90f6c148577e42a85e0236da3dba7f
ChloeLL <chloell@protonmail.ch>
2022-01-19 02:17:42 -0700

    Modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :api_keys, path: 'api-keys', only: %i[index create destroy]
+
   namespace :api do
    namespace :v1 do
      get '/forecast', to: '/api/v1/forecast#index'
      get '/backgrounds', to: '/api/v1/background#index'
+     resources :users, only: :create
    end
  end
 end
